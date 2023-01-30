08d07dbd17508fe84e487250cd4eb7ad79518401
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-01-30 17:37:55 +0700

    add global database var for test

--- database/sqlc/main_test.go
+++ database/sqlc/main_test.go
@@ -8,12 +8,15 @@ import (
 )
 
 var queriesTest *Queries
+var dbTest *sql.DB
 
 func TestMain(m *testing.M) {
 	conn, err := sql.Open("postgres", "postgres://dev:dev@localhost:5432/simple_bank?sslmode=disable")
 	if err != nil {
 		log.Fatalf("error connecting to database : %v", err)
 	}
+
+	dbTest = conn
 
 	queriesTest = New(conn)
 
